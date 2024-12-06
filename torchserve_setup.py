44074b58873df8c8a5af75b6d95acfc654c07a82
fractalhabits <fractalhabits@protonmail.com>
2024-12-06 14:54:12 -0700

    setup

---
+++ torchserve_setup.py
@@ -0,0 +1,25 @@
+from tqdm import tqdm
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
+from torch.quantization import quantize_dynamic
+
+model_name = "unitary/toxic-bert"
+
+# Using tqdm to show progress for model retrieval
+print(f'Retrieving {model_name} from transformers...')
+model = AutoModelForSequenceClassification.from_pretrained(model_name)
+
+# Simulating a longer quantization process with tqdm
+print('Quantizing model...')
+# Here we can simulate a process that takes time, for example, if we had multiple layers to quantize
+layers_to_quantize = [nn.Linear]  # Example list of layers to quantize
+for layer in tqdm(layers_to_quantize, desc="Quantizing layers"):
+    quantized_model = quantize_dynamic(model, {layer}, dtype=torch.qint8)  # Quantize each layer
+print('Quantized model created')
+
+print('Saving model')
+# Save the model
+torch.save(quantized_model, 'toxic_bert.pth')  # Directly save the model object
+print('Model saved')
